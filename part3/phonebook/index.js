a63013358fb6c2001ba52532c4e5a0f66781e2d8
Affan Mirza Chairy <affanmrz01@gmail.com>
2022-01-13 22:22:29 +0700

    done exercises 3.12 - 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const { request, response } = require('express')
+const Person = require('./models/person')
+const res = require('express/lib/response')
 const app = express()
 app.use(express.static('build'))
 app.use(express.json())
@@ -13,93 +15,109 @@ morgan.token('type', (req, res) => {
   else return ' '
 })
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
   
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(person => {
+    response.json(person)
+  })
 })
 
 app.get('/info', (request, response) => {
-  response.send(
-    `<div>
-      <p>Phonebook has info for ${persons.length} people</p>
+  Person.count({}, ( err, count) => 
+    response.send(
+      `<div>
+      <p>Phonebook has info for ${count} people</p>
       <p>${new Date()}</p>
       </div>`
-  )
+    ))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) response.json(person)
-  else response.status(404).end()
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      throw ReferenceError(`id ${request.params.id} can not be found`)
+    }
+  })
+  .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log(request.get)
-  //console.log(request.body)
+
   if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'Incomplete data' 
     })
   }
-  else if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'Similar name already exists' 
-    })
-  }
 
-  const person = {
-    id: Math.floor(Math.random() * (100000 - 1) + 1),
+  const person = new Person({
     name: body.name,
     number: body.number
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = Number(request.params.id)
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      if (result === null) {
+        throw ReferenceError(`Can not delete document with ID ${request.params.id} because the ID can not be found`)
+      }
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      if(updatedPerson === null) {
+        throw ReferenceError(`Can not update document with ID ${request.params.id} because the ID can not be found`)
+      }
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted ID' })
+  } 
+
+  else if (error.name === 'ReferenceError') {
+    return response.status(400).send({ error: 'ID not found' })
   }
 
-  persons = persons.concat(person)
+  next(error)
+}
 
-  //console.log(JSON.stringify(request.body))
-  response.json(person)
-})
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
-})
-
-app.put('/api/persons/:id', (request, response) => {
-  const body = request.body
-  person = persons.find(person => person.id === body.id)
-  person.number = body.number
-  response.json(person)
-})
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
